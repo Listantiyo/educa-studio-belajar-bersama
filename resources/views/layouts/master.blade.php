67bd0fdb24d2b57db330e2ba78887ec107d79e4e
Yasin35353535 <ay8400628@gmail.com>
2022-08-11 14:25:00 +0700

    master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,7 +25,7 @@
     <link rel="stylesheet" href="{{asset('template/pify/assets/css/style.css.map')}}">
     <link rel="stylesheet" href="{{asset('template/pify/assets/css/style.scss')}}">  
     <script src="https://kit.fontawesome.com/7f8292fccf.js" crossorigin="anonymous"></script>  
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
     
 
     <style>
