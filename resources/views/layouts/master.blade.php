d00b969227defe2dbcd8c849ad717531b2961731
Yasin35353535 <ay8400628@gmail.com>
2022-08-11 13:39:53 +0700

    edit master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,6 +25,7 @@
     <link rel="stylesheet" href="{{asset('template/pify/assets/css/style.css.map')}}">
     <link rel="stylesheet" href="{{asset('template/pify/assets/css/style.scss')}}">  
     <script src="https://kit.fontawesome.com/7f8292fccf.js" crossorigin="anonymous"></script>  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     
 
 </head>
@@ -55,6 +56,31 @@
     <script src="{{asset('template/pify/assets/js/like-dislike.min.js')}}"></script>
     <script src="{{asset('template/pify/assets/js/custom.js')}}"></script>
     {{-- <script src="{{asset('template/pify/assets/js/font-awesome-5.js')}}"></script> --}}
+
+     {{-- bootstarp --}}
+     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+     <script>
+ 
+         
+         $('.owl-carousel').owlCarousel({
+         loop:true,
+         margin: 10,
+         nav: true,
+         navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+         responsive: {
+             0: {
+                 items: 1
+             },
+             600: {
+                 items: 3
+             },
+             1000: {
+                 items: 3
+             }
+         }
+     });
+     </script>
     
 </body>
 </html>
