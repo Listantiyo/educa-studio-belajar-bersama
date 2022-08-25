9e62dd209b83233ad216bf0ed560249e8069dbe3
GedeonAS <kenzorash727@gmail.com>
2022-08-25 17:09:05 +0700

    master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -149,20 +149,20 @@
         //     width: 450
         // });
 
-        $('textarea#tiny').tinymce({
-              height: 500,
-              menubar: false,
-              plugins: [
-                'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                'insertdatetime', 'media', 'table', 'help', 'wordcount'
-                ],
-                toolbar: 'undo redo | blocks | ' +
-                'bold italic backcolor | alignleft aligncenter ' +
-                'alignright alignjustify | bullist numlist outdent indent | ' +
-                'removeformat | help',
-                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
-            });
+        // $('textarea#tiny').tinymce({
+        //       height: 500,
+        //       menubar: false,
+        //       plugins: [
+        //         'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+        //         'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+        //         'insertdatetime', 'media', 'table', 'help', 'wordcount'
+        //         ],
+        //         toolbar: 'undo redo | blocks | ' +
+        //         'bold italic backcolor | alignleft aligncenter ' +
+        //         'alignright alignjustify | bullist numlist outdent indent | ' +
+        //         'removeformat | help',
+        //         content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+        //     });
 
          $('.owl-carousel').owlCarousel({
          loop:true,
