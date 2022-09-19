98e91a08dbaf04d458e6437f4abec81dad831771
Listantiyo <listantiyowiharjanto@gmail.com>
2022-09-19 08:33:01 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,7 @@ Route::delete('menu/delete','HomeController@delete');
 // API Question
 Route::get('quest/show','QuestionController@show');
 Route::get('quest/search','QuestionController@search');
+Route::get('quest/likedislike','QuestionController@likedislike');
 Route::post('quest/store','QuestionController@store');
 Route::put('quest/update','QuestionController@update');
 Route::delete('quest/delete','QuestionController@delete');
