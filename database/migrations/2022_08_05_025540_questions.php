291ed160a2479a53e7d78a284a79496f96fa0438
Listantiyo <listantiyowiharjanto@gmail.com>
2022-09-23 08:33:06 +0700

    status quest

--- database/migrations/2022_08_05_025540_questions.php
+++ database/migrations/2022_08_05_025540_questions.php
@@ -17,6 +17,7 @@ class Questions extends Migration
             $table->increments('id');
             $table->integer('id_user_dil');
             $table->integer('id_type')->default(1);
+            $table->integer('id_status')->default(1);
             $table->integer('id_comunity');
             $table->string('title');
             $table->text('question');
